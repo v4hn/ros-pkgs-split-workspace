47d56f35040e455c983ba093b4e0c12ab9089e17
v4hn <me@v4hn.de>
2024-09-12 11:08:14 +0200

    first stab at splitting jobs

--- autosplit.py
+++ autosplit.py
@@ -4,6 +4,8 @@
 from workspace import Workspace
 from typing import Dict, Set, List, NamedTuple
 from copy import deepcopy
+import numpy as np
+import binpacking
 import sys
 
 def stages(ws):
@@ -14,10 +16,11 @@ def stages(ws):
             stage = [ws.repositories["setup_files"], ws.repositories["ros_environment"]]
         else:
             # find all repository without dependencies
-            stage = [r for r in ws.repositories.values() if not r.build_depends]
+            stage = [r for r in ws.repositories.values() if not r.build_depends and not r.exec_depends]
 
+        # TODO: deal with cycles by merging them into the same job
         if not stage:
-            print("ERROR: cyclic dependencies detected. Remaining repositories:\n", ", ".join([r.name for r in ws.repositories]))
+            print("ERROR: cyclic dependencies detected. Remaining repositories:\n", ", ".join([r for r in ws.repositories]))
             return
 
         yield stage
@@ -33,6 +36,16 @@ def stages(ws):
 if __name__ == '__main__':
     ws = Workspace(sys.argv[1] if len(sys.argv) > 1 else ".")
 
-    for i, stage in enumerate(stages(ws)):
+    stage_workers = [1, 20] + [5] * 100
+
+    # TODO: split eigenpy (and pinocchio) into separate jobs with less threads
+    for i, (stage, workers) in enumerate(zip(stages(ws), stage_workers)):
         stage_sum = sum(len(r.packages) for r in stage)
-        print(f"Stage {i}: ({stage_sum} packages / {len(stage)} repositories) {', '.join(r.name for r in stage)}")
+        jobs = binpacking.to_constant_bin_number({r.name:len(r.packages) for r in stage}, workers)
+        jobs = [j for j in jobs if j]
+        print(f"- Stage {i}\n"
+              f"  - {stage_sum} packages\n"
+              f"  - {len(stage)} repositories\n"
+              f"  - job load: " + ", ".join([str(np.sum([p for p in j.values()])) for j in jobs]))
+        for j in jobs:
+            print(f"    - {', '.join(f'{r}({p})' for r, p in j.items())}")
