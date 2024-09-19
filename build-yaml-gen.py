b301980ffada93ec0c240587eaa4165cac00fb43
v4hn <me@v4hn.de>
2024-09-20 01:26:52 +0200

    add more details to build generator

--- build-yaml-gen.py
+++ build-yaml-gen.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 
-def workers():
-  yield 1
-  yield 10
-  yield 5
-
-workers = [1, 10] + [5] * (17-2)
+required_stages = 17
+first_stages = [1, 10]
+workers = first_stages + [5] * (required_stages - len(first_stages))
 
 print("""name: build
 
@@ -13,23 +10,80 @@ on:
   workflow_dispatch:
   push:
 
-# TODO: this is incomplete and needs manual editing after generation
-jobs:""")
+env:
+  AGG: /home/runner/apt_repo_dependencies
+  DISTRIBUTION: ubuntu
+
+jobs:
+  stage-1:
+    runs-on: ubuntu-22.04
+    outputs:
+      workers: ${{ steps.worker.outputs.workers }}
+    steps:
+      - name: Check out the repo
+        uses: actions/checkout@v4
+      - name: Investigate source repositories
+        run: |
+          sudo add-apt-repository -y ppa:v-launchpad-jochen-sprickerhof-de/sbuild
+          sudo apt update
+          DEBIAN_FRONTEND=noninteractive sudo apt install -y vcstool catkin
+          mkdir src
+          vcs import --recursive --shallow --input sources.repos src
+          for PKG in $(catkin_topological_order --only-names); do
+            printf "%s:\\n  %s:\\n  - %s\\n" "$PKG" "${{ env.DISTRIBUTION }}" "ros-one-$(printf '%s' "$PKG" | tr '_' '-')" | tee -a local.yaml
+          done
+          mkdir -p ${{ env.AGG }}
+          mv local.yaml ${{ env.AGG }}/local.yaml
+          cp sources.repos ${{ env.AGG }}/sources.repos
+      - name: List used workers
+        id: worker
+        run: |
+          echo "workers=$(cat jobs.yaml | sed -n 's/^\(stage.*\):$/\\1/; T; p')" >> $GITHUB_OUTPUT
+      - name: Store meta data
+        uses: actions/cache/save@v4
+        with:
+          path: ${{ env.AGG }}
+          key: apt-repo-stage-1-${{ github.sha }}-${{ github.run_id }}-${{ github.run_attempt }}
+""", end='')
 
 for i, workers in enumerate(workers):
     for j in range(workers):
         print(f"  stage{i}-worker{j}:\n"
               f"    uses: ./.github/workflows/worker.yaml\n"
-              f"    if: success() || failure()") # TODO: skip if there is no work scheduled for this worker
-        if i > 0:
-            print(f"    needs: stage{i-1}")
-        print(f"    with:\n"
-              f"      worker: stage{i}-worker{j}")
-        if i > 0:
-            print(f"      depends: stage{i-1}")
+              f"    if: (success() || failure()) && contains( needs.stage-1.outputs.workers, 'stage{i}-worker{j}' )\n"
+              f"    needs: stage{i-1}\n"
+              f"    with:\n"
+              f"      worker: stage{i}-worker{j}\n"
+              f"      depends: stage{i-1}")
     print(f"  stage{i}:\n"
             f"    uses: ./.github/workflows/aggregate-debs.yaml\n"
             f"    if: always() && !cancelled()\n"
             f"    needs: [{', '.join([f'stage{i}-worker{j}' for j in range(workers)])}]\n"
             f"    with:\n"
             f"      stage: {i}")
+
+print("""  deploy:
+    needs: stage16
+    if: success() || failure() # can get cancelled
+    runs-on: ubuntu-22.04
+    env:
+      ROS_DISTRO: one
+      DEB_DISTRO: jammy
+    steps:
+      - name: get apt packages from last job
+        uses: actions/cache/restore@v4
+        with:
+          path: ${{ env.AGG }}
+          key: apt-repo-stage16-${{ github.sha }}-${{ github.run_id }}-${{ github.run_attempt }}
+          restore-keys: |
+            apt-repo-stage16-${{ github.sha }}-${{ github.run_id }}
+      - name: move packages to repo
+        run: |
+          mkdir -p /home/runner/apt_repo
+          mv ${{ env.AGG }}/* /home/runner/apt_repo/
+      - uses: v4hn/ros-deb-builder-action/deploy@rosotest
+        if: always()
+        with:
+          BRANCH: ${{ env.DEB_DISTRO }}-${{ env.ROS_DISTRO }}-unstable
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+          SQUASH_HISTORY: true""")
