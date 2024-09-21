92c09000dd1b631843bf7653082d49f0fbd73ca8
v4hn <me@v4hn.de>
2024-09-21 15:10:26 +0200

    comment autosplit

--- autosplit.py
+++ autosplit.py
@@ -11,8 +11,7 @@ import sys
 SBUILD_OPTIONS = {
     # limit to fewer jobs as free github runners run out of memory with defaults (determined by trial and error)
     "eigenpy": "$dpkg_buildpackage_user_options = ['--jobs=3'];",
-    # TODO: record the actual compute time of stages and use it to weight the binpacking,
-    # This hack here entails this will run in an isolated job as it's the stage1 bottleneck
+    # This entry here entails ompl will run in an isolated job as it's the stage1 bottleneck
     "ompl": "",
 }
 
@@ -43,32 +42,44 @@ if __name__ == '__main__':
     ws = Workspace(sys.argv[1] if len(sys.argv) > 1 else ".")
 
     def nr_of_workers():
+        '''
+        defines the number of workers to use for each stage in an infinite generator
+        '''
         yield 1 # built requirements cannot be parallelized
         yield 10 # the first stage contains many independent packages, but eigenpy/ompl delay it anyway
         while True: # do not excessively parallelize (though github allows 20 and possibly throttles)
             yield 5
 
     for i, (stage, workers) in enumerate(zip(stages(ws), nr_of_workers())):
+        # repositories with special sbuild options are run in isolation
+        # TODO: would fail with bonded repositories, but there are no cases of this yet
         extra_jobs = [[repo.name] for repo in stage if repo.name in SBUILD_OPTIONS]
+
+        # "regular" repositories to be assigned to workers
         repos = [repo for repo in stage if repo.name not in SBUILD_OPTIONS]
 
+        # group bonded repositories together in tasks
         tasks = dict()
         while repos:
+            # select first repository and its bonds
             repo = repos[0]
-            tasks[repo.name] = [repo]
             if repo.bonded:
                 tasks[repo.name] = [ws.repositories[br] for br in repo.bonded]
+            else:
+                tasks[repo.name] = [repo]
+
+            # remove all selected repositories from repos
             for r in tasks[repo.name]:
                 repos.pop(next(i for i, rr in enumerate(repos) if rr.name == r.name))
 
+        # TODO: record the actual compute time of stages and use it as costs instead of package count
+        # TODO: reduce used jobs by merging them to fill current maximum cost of any job
         costs = {t : sum(len(r.packages) for r in tasks[t]) for t in tasks}
         jobs = binpacking.to_constant_bin_number(costs, workers-len(extra_jobs))
-        jobs = [j for j in jobs if j] # skip empty jobs
 
-        jobs = [[repo.name for task in job for repo in tasks[task]] for job in jobs]
+        jobs = [[repo.name for task in job for repo in tasks[task]] for job in jobs if job]
 
-        # TODO: reduce jobs by merging them to fill current maximum cost of any job
-
+        # write out jobs to yaml
         for ji, job in enumerate(jobs+extra_jobs):
             repos_yaml = '[{}]'.format(', '.join(f'"{repo}"' for repo in job))
             print(f"stage{i}-worker{ji}:\n"
